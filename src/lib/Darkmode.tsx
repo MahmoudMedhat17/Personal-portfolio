0c638e20fee899590e8cdaf0bc07628388386bde
Mahmoud Medhat <M7moooud.17@gmail.com>
2025-03-03 23:16:07 +0200

    Modifying pages structure.

--- src/lib/Darkmode.tsx
+++ src/lib/Darkmode.tsx
@@ -12,16 +12,15 @@ const Darkmode = () => {
 
       if(typeof window !== undefined){
         localStorage.setItem("theme", theme);
-      }
-
-      if(theme === "dark"){
-        document.documentElement.classList.add("dark");
-        document.documentElement.classList.remove("light");
-      }
-      else{
-        document.documentElement.classList.add("light");
-        document.documentElement.classList.remove("dark");
-      }
+        if(theme === "dark"){
+          document.documentElement.classList.add("dark");
+          document.documentElement.classList.remove("light");
+        }
+        else{
+          document.documentElement.classList.add("light");
+          document.documentElement.classList.remove("dark");
+        }
+      };
     },[theme]);
 
 
