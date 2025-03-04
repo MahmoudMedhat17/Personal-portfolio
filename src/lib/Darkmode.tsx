286e37a3234066ddfa601d8b5b217735010a5b66
Mahmoud Medhat <M7moooud.17@gmail.com>
2025-03-04 02:23:32 +0200

    Modifying file structure.

--- src/lib/Darkmode.tsx
+++ src/lib/Darkmode.tsx
@@ -1,47 +1,39 @@
 "use client";
 
+import {useState, useEffect} from 'react';
+import { GoSun } from "react-icons/go";
+import { IoMoonOutline } from "react-icons/io5";
 
-import { CiDark, CiLight } from "react-icons/ci";
-import { useEffect, useState } from "react";
 
 const Darkmode = () => {
 
-    const [theme,setTheme] = useState<string | null>(null);
-
-    useEffect(()=>{
-     if(typeof window !== undefined){
-      const storedTheme = localStorage.getItem("theme") || "light";
-      setTheme(storedTheme);
-     }
-    },[]);
-
-    useEffect(()=>{
-
-      if(typeof window !== undefined && theme){
-        localStorage.setItem("theme", theme);
-
-        if(theme === "dark"){
-          document.documentElement.classList.add("dark");
-          document.documentElement.classList.remove("light");
-        }
-        else{
-          document.documentElement.classList.add("light");
-          document.documentElement.classList.remove("dark");
-        }
-      };
-    },[theme]);
+  const [theme,setTheme] = useState(localStorage.getItem("theme") || "light");
 
 
-    const handleDarkMode = () =>{
-      setTheme((prev)=> (prev === "dark" ? "light" : "dark"));
+  useEffect(()=>{
+    localStorage.setItem("theme", theme);
+
+    if(theme === "dark"){
+      document.documentElement.classList.add("dark");
+      document.documentElement.classList.remove("light");
     }
-  
+    else{
+      document.documentElement.classList.add("light");
+      document.documentElement.classList.remove("dark");
+    }
+  },[theme]);
+
+
+  const handleDarkMode = () =>{
+    setTheme((prev)=> prev === "dark" ? "light" : "dark");
+  }
+
   return (
-    <div className="flex items-center relative">
-        <CiLight onClick={handleDarkMode} size={30} className={`absolute ${theme === "dark" ? "opacity-0" : "opacity-100"} cursor-pointer dark:text-lightModeColor text-darkModeColor`}/>
-        <CiDark  onClick={handleDarkMode} size={30} className={`absolute ${theme === "light" ? "opacity-0" : "opacity-100"} cursor-pointer dark:text-lightModeColor text-darkModeColor`}/>
+    <div className='relative flex items-center'>
+      <GoSun size={20} onClick={handleDarkMode} className={`dark:text-lightModeColor text-darkModeColor absolute cursor-pointer ${theme === "dark" ? "opacity-0" : "opacity-100"}`}/>
+      <IoMoonOutline size={20} onClick={handleDarkMode} className={`dark:text-lightModeColor text-darkModeColor absolute cursor-pointer ${theme === "light" ? "opacity-0" : "opacity-100"}`}/>
     </div>
   )
-};
+}
 
 export default Darkmode;
